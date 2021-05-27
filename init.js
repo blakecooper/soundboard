3887bb66b592e7b34a1031b16466776f4f090f27
Blake Cooper <blakecoo@buffalo.edu>
2021-05-26 20:44:00 -0400

    Buttons have labels, background colors

--- init.js
+++ init.js
@@ -86,13 +86,33 @@ window.onload = function()
     }
 }
 
-    for (var i = 0; i < data.length; i++) {
-        document.getElementById("container").innerHTML += "<canvas id='canvas" + i + "' width='" + window.innerWidth + "' height='" + window.innerHeight/14 + "'" + data[i].image + ".jpg');\" onclick='play(" + i + ")'>" + data[i].text + "</canvas>";
-        
-        var c = document.getElementById("canvas" + i);
+for (var i = 0; i < data.length; i++) {
+
+    document.getElementById("container").innerHTML += "<canvas id='canvas" + i + "' width='" + window.innerWidth/5 + "' height='" + window.innerHeight/3 + "' onclick='play(" + i + ")'>" + data[i].text + "</canvas>";
+
+}
+
+for (var i = 0; i < data.length; i++) {
+
+
+
+        var canvasId = "canvas" + i;
+        console.log(canvasId);
+        var c = document.getElementById(canvasId);
+        console.log(c);
         var ctx = c.getContext("2d");
-        var img = document.getElementById(data[i].image);
-        ctx.drawImage(img,10,10);
-        ctx.font = "30px Arial";
-        ctx.fillText("Hello World", 10, 50);
+        console.log(ctx);
+        ctx.globalAlpha = 0.3;
+        ctx.fillStyle = "#0000FF";
+        ctx.fillRect(0, 0, c.width, c.height);
+//        var img = new Image;
+//        img.onload = function() {
+//            ctx.drawImage(img, 20,20);
+//            URL.revokeObjectURL(img.src)
+//        }
+//        img.src = "images/" + data[i].image + ".jpg";
+        ctx.globalAlpha = 1;
+        ctx.fillstyle = "#000000"; 
+        ctx.font = "20px Arial";
+ctx.fillText(data[i].text, c.width/3, c.height/2);
 }
